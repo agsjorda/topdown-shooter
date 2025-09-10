5c22423e9a6f01690a72463ba579a79c7d9061f3
agsjorda <42771722+agsjorda@users.noreply.github.com>
2025-09-10 12:50:35 +0800

    added weapon animation layers for shotgun and sniper rifle

---
+++ Assets/LeftHandTargetTransform.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour {
+    //We need this script to make sure the left hand target transform follows the gun's left hand target position and rotation
+}
--- Assets/WeaponVisualController.cs
+++ Assets/WeaponVisualController.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 public class WeaponVisualController : MonoBehaviour {
+
+    private Animator anim;
+
     [SerializeField] private Transform[] gunTransforms;
 
     [SerializeField] private Transform pistol;
@@ -9,31 +12,66 @@ public class WeaponVisualController : MonoBehaviour {
     [SerializeField] private Transform shotgun;
     [SerializeField] private Transform rifle;
 
+    private Transform currentGun;
 
+    [Header("Left and IK")]
+    [SerializeField] private Transform leftHand;
+
+    private void Start() {
+        SwitchOn(pistol);
+
+        anim = GetComponentInParent<Animator>();
+    }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
             SwitchOn(pistol);
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchAnimationLayer(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
             SwitchOn(revolver);
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SwitchAnimationLayer(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
             SwitchOn(autoRifle);
-        if (Input.GetKeyDown(KeyCode.Alpha4))
+            SwitchAnimationLayer(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
             SwitchOn(shotgun);
-        if (Input.GetKeyDown(KeyCode.Alpha5))
+            SwitchAnimationLayer(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) {
             SwitchOn(rifle);
+            SwitchAnimationLayer(3);
+        }
 
     }
 
     private void SwitchOn(Transform gunTransform) {
         SwitchOffGuns();
         gunTransform.gameObject.SetActive(true);
+        currentGun = gunTransform;
+
+        AttachLeftHand();
     }
 
     private void SwitchOffGuns() {
 
         for (int i = 0; i < gunTransforms.Length; i++) {
             gunTransforms[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void AttachLeftHand() {
+        Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
+
+        leftHand.localPosition = targetTransform.localPosition;
+        leftHand.localRotation = targetTransform.localRotation;
+    }
+
+    private void SwitchAnimationLayer(int layerIndex) {
+        for (int i = 1; i < anim.layerCount; i++) {
+            anim.SetLayerWeight(i, i == layerIndex ? 1 : 0);
         }
     }
 }
